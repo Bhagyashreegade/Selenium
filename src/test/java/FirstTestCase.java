0c440c4cc6ed65835843effbf282a2d6fa90394b
Bhagyashree Gade <bhagyashreegade91@gmail.com>
2022-11-11 10:45:20 -0500

    enabled & disabled

--- src/test/java/FirstTestCase.java
+++ src/test/java/FirstTestCase.java
@@ -5,18 +5,19 @@ public class FirstTestCase {
     /*Test Suites--> Test Cases--> Test steps
     Package--->Classes---> Methods*/
 
-    @Test(priority = 1)
-   public static void setup(){
-       System.out.println("opening browser");
-   }
-   @Test (priority = 2)
-   public static void login(){
-       System.out.println("Login into browser");
-   }
+    @Test
+    public static void setup() {
+        System.out.println("opening browser");
+    }
 
-   @Test(priority = 3)
-    public static void teardown(){
-       System.out.println("closing the browser");
-   }
+    @Test    //By default all Tcs are enabled
+    public static void login() {
+        System.out.println("Login into browser");
+    }
+
+    @Test
+    public static void teardown() {
+        System.out.println("closing the browser");
+    }
 
 }
