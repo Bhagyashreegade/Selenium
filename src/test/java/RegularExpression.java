4b43c29b3027b3d981e1709258ad28fddef468e3
Bhagyashree Gade <bhagyashreegade91@gmail.com>
2022-11-11 15:09:49 -0500

    Attributes of @Test

--- src/test/java/RegularExpression.java
+++ src/test/java/RegularExpression.java
@@ -2,27 +2,27 @@ import org.testng.annotations.Test;
 
 public class RegularExpression {
 
-    @Test
+    @Test(description = "This is a WebLogin car loan test")
     public void WebLoginCarLoan() {
         System.out.println("WebLoginCarLoan");
     }
 
-    @Test
+    @Test (priority = 1)
     public void MobileLoginCarLoan() {
         System.out.println("MobileLoginCarLoan");
     }
 
-    @Test
+    @Test (timeOut = 3)
     public void MobileLoginPersonalLoan() {
         System.out.println("MobileLoginPersonalLoan");
     }
 
-    @Test
+    @Test (dependsOnMethods = {"WebLoginCarLoan"})
     public void MobileLoginHomeLoan() {
         System.out.println("MobileLoginHomeLoan");
     }
 
-    @Test
+    @Test (priority = 2)
     public void LoginAPICarLoan() {
         System.out.println("LoginAPICarLoan");
     }
