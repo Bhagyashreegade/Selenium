78fdbbf59e370a60a687bce50a649d4f0c42981c
Bhagyashree Gade <bhagyashreegade91@gmail.com>
2022-11-11 14:42:01 -0500

    Before Annotations

--- src/test/java/FirstTestCase.java
+++ src/test/java/FirstTestCase.java
@@ -1,3 +1,6 @@
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class FirstTestCase {
@@ -5,18 +8,33 @@ public class FirstTestCase {
     /*Test Suites--> Test Cases--> Test steps
     Package--->Classes---> Methods*/
 
+    @BeforeClass
+    public void dataPrinterBeforeClass() {
+        System.out.println("Before Class of FirstTestCase");
+    }
+
+    @BeforeSuite
+    public void dataPrinterBeforeMethod() {
+        System.out.println("Before Suite");
+    }
+
+    @BeforeTest
+    public void dataPrinterBeforeGroup() {
+        System.out.println("Before Test");
+    }
+
     @Test
-    public static void setup() {
+    public void setup() {
         System.out.println("opening browser");
     }
 
     @Test    //By default all Tcs are enabled
-    public static void login() {
+    public void login() {
         System.out.println("Login into browser");
     }
 
     @Test
-    public static void teardown() {
+    public void teardown() {
         System.out.println("closing the browser");
     }
 
--- src/test/java/Home_Loan.java
+++ src/test/java/Home_Loan.java
@@ -1,6 +1,12 @@
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class Home_Loan {
+
+    @BeforeClass
+    public void testData(){
+        System.out.println("Before Class of HomeLoan");
+    }
 
     @Test
     public void WebLoginHomeLoan() {
